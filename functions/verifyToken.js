3269781a838124465f3114b829da73d6203a815e
Imane <imaneelmouradi98@gmail.com>
2019-07-26 21:30:32 +0100

    token key

--- functions/verifyToken.js
+++ functions/verifyToken.js
@@ -1,13 +1,11 @@
 // a middleware for token verification
 
-const smssender_token = "test";
-
 const verifyToken = (req, res, next) => {
   const token = req.header("smsender_token");
   if (!token) return res.status(401).send("Access denied");
 
   try {
-    if (token === smssender_token) {
+    if (token === process.env.smsToken) {
       next();
     }
   } catch {
